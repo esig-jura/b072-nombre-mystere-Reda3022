f5ce6f69d7b3d2231886ff94c590eb6f76eb8c5b
ahmed.saci <ahmed.saci@divcom.ch>
2023-02-08 11:11:29 +0100

    correction

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -20,36 +20,53 @@ function tireNombre(min, max) {
 // TODO Attention jamais plus de 2 lignes vides à la suite
 
 
-const mini = 1; // TODO Nom des constantes de valeur en majuscule mini => MINI.
-const maxi = 100; // TODO Ajouter des espaces avant et après le signe =
+const MINI = 1; // TODO Nom des constantes de valeur en majuscule mini => MINI.
+const MAXI = 100; // TODO Ajouter des espaces avant et après le signe =
 
-let nbEssai=1;
-let stop=true; // TODO Supprimer
-let nbMistere = tireNombre(mini, maxi);
+let nbEssai = 0;
+// let stop=true; // TODO Supprimer
+let nbMistere = tireNombre(MINI, MAXI);
+let message  = ' entrer un nombre entre '+ MINI +' et '+ MAXI ;
+let reponse = null;
+
 // TODO Déclarer réponse et mettre à null
 // juste pour voir cest quoi le numero secret
 alert(nbMistere);
 
-while (stop){ // TODO do..while
+// while (stop){ // TODO do..while
+//
+//     let reponse= Number(prompt('entrer un nombre entre 1 et 100 :'));
+//
+//     if (reponse<nbMistere){
+//
+//         alert('plus petit');
+//     } else if (reponse > nbMistere){
+//
+//         alert('plus grand');
+//
+//     } else {
+//
+//         alert('vous avez trouvé le bon numero apres '+nbEssai +' essai ');
+//         stop=false;
+//
+//     }
+//
+//     nbEssai++;
+// }
 
-    let reponse= Number(prompt('entrer un nombre entre 1 et 100 :'));
+do {
+    reponse= Number(prompt(message));
 
-    if (reponse<nbMistere){
-
-        alert('plus petit');
-    } else if (reponse > nbMistere){
-
-        alert('plus grand');
-
+    if (reponse < nbMistere){
+        message = 'le numero entrer il est plus petit ! ';
     } else {
-
-        alert('vous avez trouvé le bon numero apres '+nbEssai +' essai ');
-        stop=false;
-
+        message = 'le numero entrer il est plus grand ! ';
     }
 
     nbEssai++;
-}
+} while (reponse !== nbMistere);
+
+alert('vous avez trouvé le bon numero apres '+ nbEssai + ' essai ');
 
 
 
