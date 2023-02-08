e416dd53a3d195f2ea8e95cfef1b9190331831e1
ahmed.saci <ahmed.saci@divcom.ch>
2023-02-08 10:29:56 +0100

    correction

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,14 +1,69 @@
 /**
  * Jeu du nombre mystère
- * @author  Steve Fallet
+ * @author  Reda Saci
  * @version 1.0
- * @since   2022-08-30 (date de création)
+ * @since   2023-02-08 (date de création)
  */
 
-//Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
-(function main() {
-    'use strict';
+'use strict';
+// TODO afficher les messages d'erreur en HTML et pas avec alert
+// TODO panneau de config pour modifer MIN et MAX
 
-   // Ton code de malade ici ...
 
-}()); //main IIFE
+
+
+
+
+
+
+
+/**
+ * Fonction qui retourne un nombre aléatoire compris entre min et max
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+function tireNombre(min, max) {
+
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+const mini=1;
+const maxi=100;
+
+let nbEssai=1;
+
+let stop=true;
+
+let nbMistere=tireNombre(mini,maxi);
+
+// juste pour voir cest quoi le numero secret
+alert(nbMistere);
+
+while (stop){
+
+    let reponse= Number(prompt('entrer un nombre entre 1 et 100 :'));
+
+    if (reponse<nbMistere){
+
+        alert('plus petit');
+    }else if (reponse>nbMistere){
+
+        alert('plus grand');
+
+    }else{
+
+        alert('vous avez trouvé le bon numero apres '+nbEssai +' essai ');
+        stop=false;
+
+    }
+
+    nbEssai++;
+
+
+}
+
+
+
+
